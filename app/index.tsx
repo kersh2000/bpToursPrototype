55c6626f4c3a01e18810e3262018c7288d8c54c0
kersh2000 <bradleykershaw333@gmail.com>
2025-03-03 15:30:01 +0000

    Add basic styling to homepage for navigation.

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "expo-router";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 
 // React component
 const Index = () => {
@@ -7,12 +7,49 @@ const Index = () => {
         <View
             style={styles.container}
         >
-            <Link style={styles.pageLink} href="/about">About Us</Link>
-            <Link style={styles.pageLink} href="/timetable">Bus Timetable</Link>
-            <Link style={styles.pageLink} href="/events">Guided Events</Link>
-            <Link style={styles.pageLink} href="/reviews">Reviews Page</Link>
-            <Link style={styles.pageLink} href="/account">Account Page</Link>
-            <Link style={styles.pageLink} href="/auth">Authentication Page</Link>
+            <Link 
+                style={styles.account} 
+                href="/auth"
+            >
+                Account{"\n"}Login
+            </Link>
+            <Image 
+                source={require('./img/bp-tours-logo.png')}
+                style={styles.companyImage}
+            />
+            <Text 
+                style={styles.compnayText}
+            >
+                This is the dcescription of the BP Tours company blah blah balh...
+            </Text>
+            <View 
+                style={styles.pageContainer}
+            >
+                <Link 
+                    style={styles.pageLink} 
+                    href="/about"
+                >
+                    About Us
+                </Link>
+                <Link 
+                    style={styles.pageLink} 
+                    href="/timetable"
+                >
+                    Bus Timetable
+                </Link>
+                <Link 
+                    style={styles.pageLink} 
+                    href="/events"
+                >
+                    Guided Events
+                </Link>
+                <Link 
+                    style={styles.pageLink} 
+                    href="/reviews"
+                >
+                    Reviews Page
+                </Link>
+            </View>
         </View>
     );
 }
@@ -22,12 +59,50 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "flex-start",
-        alignItems: "center"
+        alignItems: "center",
+        margin: 5,
+        borderWidth: 1
+    },
+    companyImage: {
+        height: '25%',
+        width: '90%',
+        borderWidth: 4,
+        margin: 4
+    },
+    compnayText: {
+        backgroundColor: 'orange',
+        margin: 4
+    },
+    pageContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: "flex-start",
+        justifyContent: 'center',
+        margin: 5,
+        borderWidth: 1
     },
     pageLink: {
-        textAlign: 'center'
+        width: '35%',
+        aspectRatio: 1,
+        alignSelf: 'center',
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        backgroundColor: 'lightgreen',
+        margin: 8,
+        borderWidth: 2,
+        borderRadius: 8,
+        elevation: 5
+    },
+    account: {
+        alignSelf: 'flex-end',
+        textAlign: 'center',
+        margin: 8,
+        padding: 2,
+        borderWidth: 2,
+        borderRadius: 10,
+        backgroundColor: 'lightgrey'
     }
-})
+});
 
 // React component export
 export default Index;
