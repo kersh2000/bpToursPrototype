731bf9d80537de8b37316739a97331aa8e91dca6
kersh2000 <bradleykershaw333@gmail.com>
2025-03-03 15:19:56 +0000

    Add navigation system with expo-router and stacks.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{ title: 'Home' }} />
+            <Stack.Screen name="about" options={{ title: 'About' }} />
+            <Stack.Screen name="account" options={{ title: 'Account' }} />
+            <Stack.Screen name="events" options={{ title: 'Events' }} />
+            <Stack.Screen name="reviews" options={{ title: 'Reviews' }} />
+            <Stack.Screen name="timetable" options={{ title: 'Timetable' }} />
+            <Stack.Screen name="auth" options={{ title: 'User Login' }} />
+        </Stack>
+    )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+// React component
+const Index = () => {
+    return (
+        <View
+            style={styles.container}
+        >
+            <Link style={styles.pageLink} href="/about">About Us</Link>
+            <Link style={styles.pageLink} href="/timetable">Bus Timetable</Link>
+            <Link style={styles.pageLink} href="/events">Guided Events</Link>
+            <Link style={styles.pageLink} href="/reviews">Reviews Page</Link>
+            <Link style={styles.pageLink} href="/account">Account Page</Link>
+            <Link style={styles.pageLink} href="/auth">Authentication Page</Link>
+        </View>
+    );
 }
+
+// React component styling
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "center"
+    },
+    pageLink: {
+        textAlign: 'center'
+    }
+})
+
+// React component export
+export default Index;
